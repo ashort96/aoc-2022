55fd6e2f007c97b0b8644173d9b3faa0cdcbba18
Adam Short <adam.short@gemini.com>
2022-12-04 14:12:22 -0500

    Refactor

--- day04/src/main/scala/Main.scala
+++ day04/src/main/scala/Main.scala
@@ -3,21 +3,32 @@ import scala.io.Source
 object Main extends App {
   val filename = "input.txt"
 
-  def findOverlap(lines: List[List[List[String]]], callback: (Set[Int], Set[Int]) => Int) = {
+  def findOverlap(lines: List[List[List[Int]]], callback: (Set[Int], Set[Int]) => Int) = {
     lines.map { line =>
       line match {
         case List(List(a, b), List(c, d)) =>
-          val left = a.toInt.to(b.toInt).toSet
-          val right = c.toInt.to(d.toInt).toSet
+          val left = a.to(b).toSet
+          val right = c.to(d).toSet
           callback(left, right)
+        case List(_) | Nil => throw new RuntimeException("Unexpected case")
       }
     }.sum
   }
   
-  val lines = Source.fromResource(filename)
+  val lines: List[List[List[Int]]] = Source.fromResource(filename)
     .getLines()
     .toList
-    .map(line => line.split(',').toList.map(element => element.split('-').toList))
+    .map{ line => 
+      line
+      .split(',')
+      .toList
+      .map { element => 
+        element
+        .split('-')
+        .map(_.toInt)
+        .toList
+      }
+    }
   
   val part1 = findOverlap(lines, (left: Set[Int], right: Set[Int]) => {
     if (left.subsetOf(right) || right.subsetOf(left)) 1 else 0
